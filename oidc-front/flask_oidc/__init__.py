35c3e63e2b765b0d47a135512e9b1c1bf2a201f2
Mohamed Belazar <b@kwop.org>
2021-06-09 23:23:43 +0200

    add proxy capapility

--- oidc-front/flask_oidc/__init__.py
+++ oidc-front/flask_oidc/__init__.py
@@ -738,7 +738,7 @@ class OpenIDConnect(object):
             logger.debug("CSRF token mismatch")
             return True, self._oidc_error()
 
-        proxy_info = False
+        proxy_info = None
         if current_app.config['PROXY_DOMAIN']:
             proxy_info = httplib2.ProxyInfo(proxy_type=httplib2.socks.PROXY_TYPE_HTTP,
                                             proxy_host=current_app.config['PROXY_DOMAIN'],
@@ -746,7 +746,7 @@ class OpenIDConnect(object):
 
         # make a request to IdP to exchange the auth code for OAuth credentials
         flow = self._flow_for_request()
-        credentials = flow.step2_exchange(code, httplib2.Http(proxy_info=proxy_info))
+        credentials = flow.step2_exchange(code=code, http=httplib2.Http(proxy_info=proxy_info))
         id_token = credentials.id_token
         if not self._is_id_token_valid(id_token):
             logger.debug("Invalid ID token")
